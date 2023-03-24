3b995bb89061125f013288681e94041ba62c6a3b
WESLEY PLAZA <69492841+wapdrums@users.noreply.github.com>
2023-03-24 18:24:00 -0300

    Add files via upload

---
+++ Aula11-JavaScript/Anotacoes.js
@@ -0,0 +1,8 @@
+console.log('o console funcionou corretamente')
+
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}km/H`)
+
+console.log('Digija sempre usando cinto de segurança')
+
+console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
---
+++ Aula11-JavaScript/ex008.js
@@ -0,0 +1,16 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade de ${vel} e será multado`)
+    //até aqui é uma condição simples
+} else {
+    console.log('parabens, você não ultrapassou a velocidade permitida')
+} // até aqui é uma condição composta
+
+
+
+
+
+console.log('Diriga sempre usando cinto de segunraça')
+// organização do codigo = identação
